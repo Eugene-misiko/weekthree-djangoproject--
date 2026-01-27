158bcba103b15ca4c2041ad0700aef450cc15315
Eugene <eugenemisiko438@gmail.com>
2026-01-27 23:27:15 +0300

    created views for the posts for the user

--- posts/views.py
+++ posts/views.py
@@ -6,3 +6,14 @@ from .models import Post
 def feed_view(request):
     posts = Post.objects.all().order_by('_created_at')
     return render(request, 'posts/feed.html', {'posts': posts})
+
+@login_required
+def create_post_view(request):
+    if request.method == 'POST':
+        Post.objects.create(
+            author = request.user,
+            content = request.POST['content'],
+            image = request.FILES.get('image')
+        )
+        return redirect('feed')
+    return render(request, 'posts/create_post.html')
