a8b042fe245d1a884ccc8e36fcbc92dbb88386c1
Eugene <eugenemisiko438@gmail.com>
2026-01-28 01:42:58 +0300

    created a template for user serch

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register_view, login_view, logout_view, profile_view, follow_user, unfollow_user
+from .views import register_view, login_view, logout_view, profile_view, follow_user, unfollow_user,user_search_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('profile/<int:user_id>/', profile_view, name='profile'),
     path('profile/<int:user_id>/follow/', follow_user, name='follow'),
-    path('profile/<int:user_id>/unfollow/', unfollow_user, name='unfollow'),    
+    path('profile/<int:user_id>/unfollow/', unfollow_user, name='unfollow'),
+    path('search/', user_search_view, name='user_search'),    
 ]
