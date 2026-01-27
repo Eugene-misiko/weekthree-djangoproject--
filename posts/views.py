dbc5d8e9d787e2094a4e56c23e92cb5144675269
Eugene <eugenemisiko438@gmail.com>
2026-01-27 23:17:57 +0300

    created a views for feed that showcase a particular time the content is being post and orderd according to the time

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Post
 # Create your views here.
+@login_required
+def feed_view(request):
+    posts = Post.objects.all().order_by('_created_at')
+    return render(request, 'posts/feed.html', {'posts': posts})
