e3bdbaf7e30d9a1d670bab79b63091289468fcef
Eugene <eugenemisiko438@gmail.com>
2026-01-27 23:10:24 +0300

    created model for post and it's field including likes, image field and time field

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+
+    def __str__(self):
+        return f"{self.author.username} - {self.content[:20]}"
